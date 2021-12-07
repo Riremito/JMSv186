51262060fbee90eb0bffe6194596408069b15a5f
Riremito <29014872+Riremito@users.noreply.github.com>
2021-12-08 01:47:01 +0900

    不要な設定ファイル削除

--- JMSv186/dump_WZstring.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-@title WZStringDumper
-set CLASSPATH=.;scripts\java\MapleSEA.jar;scripts\java\libs\*
-java -Dnet.sf.odinms.wzpath=extensible\ tools.WZStringDumper strings
-pause
--- JMSv186/launch_main_server_test.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-@title かえでサーバー
-set CLASSPATH=.;dist\*
-java -server -Dnet.sf.odinms.wzpath=wz\ -Djavax.net.ssl.keyStore=filename.keystore -Djavax.net.ssl.keyStorePassword=passwd -Djavax.net.ssl.trustStore=filename.keystore -Djavax.net.ssl.trustStorePassword=passwd server.Start 185 0
-pause
