a8f854eadbd373b1fe44ed4518c3049cc756e889
Riremito <29014872+Riremito@users.noreply.github.com>
2024-02-19 00:12:01 +0900

    cms v85 headers

--- src/packet/CMS_v85_1_CP.java
+++ src/packet/CMS_v85_1_CP.java
@@ -18,6 +18,8 @@
  */
 package packet;
 
+import packet.ClientPacket.Header;
+
 /**
  *
  * @author Riremito
@@ -25,14 +27,24 @@ package packet;
 public class CMS_v85_1_CP {
 
     public static void Set() {
-        ClientPacket.Header.CP_CheckPassword.Set(0x0001);
-        ClientPacket.Header.CP_WorldInfoRequest.Set(0x0002);
-        ClientPacket.Header.CP_CheckUserLimit.Set(0x0005);
-        ClientPacket.Header.CP_SelectWorld.Set(0x0009);
-        ClientPacket.Header.CP_SelectCharacter.Set(0x000A);
-        ClientPacket.Header.CP_MigrateIn.Set(0x000B);
-        ClientPacket.Header.CP_CheckDuplicatedID.Set(0x000C);
-        ClientPacket.Header.CP_CreateNewCharacter.Set(0x0011);
-        ClientPacket.Header.CP_CreateSecurityHandle.Set(0x0020);
+        Header.CP_CheckPassword.Set(0x0001);
+        Header.CP_WorldInfoRequest.Set(0x0002);
+        Header.CP_CheckUserLimit.Set(0x0005);
+        Header.CP_SelectWorld.Set(0x0009);
+        Header.CP_SelectCharacter.Set(0x000A);
+        Header.CP_MigrateIn.Set(0x000B);
+        Header.CP_CheckDuplicatedID.Set(0x000C);
+        Header.CP_CreateNewCharacter.Set(0x0011);
+        Header.CP_CreateSecurityHandle.Set(0x0020);
+
+        // test
+        Header.CP_UserChat.Set(0x0032);
+        Header.CP_UserSelectNpc.Set(0x003B);
+        Header.CP_UserPortalScriptRequest.Set(0x006C);
+        Header.CP_UserPortalTeleportRequest.Set(Header.CP_UserPortalScriptRequest.Get() + 1);
+
+        Header.CP_UserTransferFieldRequest.Set(0x0023);
+        Header.CP_UserTransferChannelRequest.Set(Header.CP_UserTransferFieldRequest.Get() + 1);
+        Header.CP_UserMigrateToCashShopRequest.Set(Header.CP_UserTransferFieldRequest.Get() + 2);
     }
 }
--- src/packet/CMS_v85_1_SP.java
+++ src/packet/CMS_v85_1_SP.java
@@ -18,6 +18,8 @@
  */
 package packet;
 
+import packet.ServerPacket.Header;
+
 /**
  *
  * @author Riremito
@@ -25,16 +27,36 @@ package packet;
 public class CMS_v85_1_SP {
 
     public static void Set() {
-        ServerPacket.Header.LP_CheckPasswordResult.Set(0x0000);
-        ServerPacket.Header.LP_CheckUserLimitResult.Set(0x0006);
-        ServerPacket.Header.LP_WorldInformation.Set(0x0009);
-        ServerPacket.Header.LP_SelectWorldResult.Set(0x000A);
-        ServerPacket.Header.LP_SelectCharacterResult.Set(0x000B);
-        ServerPacket.Header.LP_CheckDuplicatedIDResult.Set(0x000C);
-        ServerPacket.Header.LP_CreateNewCharacterResult.Set(0x0011);
-        ServerPacket.Header.LP_DeleteCharacterResult.Set(0x0012);
-        ServerPacket.Header.LP_MigrateCommand.Set(0x0018);
-        ServerPacket.Header.LOGIN_AUTH.Set(0x001F);
-        ServerPacket.Header.LP_SetField.Set(0x008A);
+        // Login
+        Header.LP_CheckPasswordResult.Set(0x0000);
+        Header.LP_CheckUserLimitResult.Set(0x0006);
+        Header.LP_WorldInformation.Set(0x0009);
+        Header.LP_SelectWorldResult.Set(0x000A);
+        Header.LP_SelectCharacterResult.Set(0x000B);
+        Header.LP_CheckDuplicatedIDResult.Set(0x000C);
+        Header.LP_CreateNewCharacterResult.Set(0x0011);
+        Header.LP_DeleteCharacterResult.Set(0x0012);
+        Header.LP_MigrateCommand.Set(0x0018);
+        Header.LOGIN_AUTH.Set(0x001F);
+
+        // 0021 - 0089 CWvsContext::OnPacket
+        // 008A - 008C CStage::OnPacket
+        Header.LP_SetField.Set(0x008A);
+        Header.LP_SetITC.Set(Header.LP_SetField.Get() + 1);
+        Header.LP_SetCashShop.Set(Header.LP_SetField.Get() + 2);
+
+        // CField::OnPacket
+        // CNpcPool::OnPacket
+        Header.LP_ImitatedNPCData.Set(0x005D);
+        Header.LP_LimitedNPCDisableInfo.Set(0x005E);
+        Header.LP_NpcEnterField.Set(0x012D);
+        Header.LP_NpcLeaveField.Set(0x012E);
+        Header.LP_NpcChangeController.Set(0x012F);
+        Header.LP_NpcMove.Set(0x0130);
+        Header.LP_NpcUpdateLimitedInfo.Set(0x0131);
+        Header.LP_NpcSpecialAction.Set(0x0133);
+        Header.LP_NpcSetScript.Set(0x0134);
+        // CScriptMan::OnPacket
+        Header.LP_ScriptMessage.Set(0x0174);
     }
 }
